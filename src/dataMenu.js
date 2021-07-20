30b45d128215acd7596b768fbbd5dbec40eeada6
SeungMinK <ksm8797@naver.com>
2021-07-20 22:01:59 +0900

    addCode - 코드 모듈화

---
+++ src/dataMenu.js
@@ -0,0 +1,23 @@
+export default[
+
+{
+    id :0,
+    title : "봉골레 파스타",
+    price : 15000
+},
+
+{   
+    id :1,
+    title : "토마토 파스타",
+    price : 13000
+
+},
+
+{
+    id:2,
+    title : "크림 파스타",
+    price : 16000
+}
+
+
+]
