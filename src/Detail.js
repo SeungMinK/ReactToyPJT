5077170c373aed366137710064de04dd91af8109
SeungMinKim <ksm8797@naver.com>
2021-07-23 11:13:42 +0900

    input으로 값 입력받고 바로 State에 저장하기

--- src/Detail.js
+++ src/Detail.js
@@ -17,7 +17,9 @@ function Detail(p){//모듈화
 
     let {id} = useParams();
     let history = useHistory();//이동햇던 방문기록을 저장해놓는 object
-    let [Ui,UiChange] = useState(true);
+    let [Ui,UiChange] = useState(true);//UI Switch
+    let [inputData,inputDataChange] = useState('');
+
     useEffect(()=>{
 
         setTimeout(()=>{UiChange(false)},2000);
@@ -33,6 +35,15 @@ function Detail(p){//모듈화
                  {Ui == true ? <h2>찾아오시는길 안내 입니다.</h2> : <h2>찾아오는 방법 안내 입니다.</h2>}   
                 <span>5호선 강일역에 내리셔서 3번 출구로 100m 직진 후 2층 상가에 위치하고있습니다.</span>
             </div>
+
+            <input onChange={(e)=>{inputDataChange(e.target.value)}}/>
+            {/*
+            여기서 onChnge()는 input 박스가 바뀔때 마다 실행하는 함수
+            e는 이벤트 객체
+            e.target.value => input에 현재 입력된 값
+            */}
+            {inputData}
+            
             <button onClick = {()=>{
                 //history.goBack();//goBack()함수와 같은 함수들의 사용법은 라이브러리 문서를 참고
                 history.push('/')//원하는 경로로 이동
